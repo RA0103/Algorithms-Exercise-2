35b20650817c39d75fed4a422c81bf70247d1df2
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2022-01-16 10:06:26 +0500

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+// Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+
+// Examples
+
+// frames(1, 1) ➞ 60
+
+// frames(10, 1) ➞ 600
+
+// frames(10, 25) ➞ 15000
+
+function frames (minutes, fps) {
+    return ((minutes * 60) * fps)
+}
+console.log(frames(10,25));
